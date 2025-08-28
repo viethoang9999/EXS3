c5e4d005ed299f91828e0cb2370482a881ec79a4
Viet_Hoang_1105 <163992939+viethoang9999@users.noreply.github.com>
2025-08-28 11:20:02 +0700

    Update User.java

--- src/main/java/murach/business/User.java
+++ src/main/java/murach/business/User.java
@@ -6,22 +6,18 @@ public class User implements Serializable {
     private String firstName;
     private String lastName;
     private String email;
-    
-    public User() {
-        firstName = "";
-        lastName = "";
-        email = "";
-    }
-    
-    public User(String firstName, String lastName, String email) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-    }
+    private String dob;
+    private String source;
+    private String offers;
+    private String emailOffers;
+    private String contactMethod;
+
+    public User() {}
+
+    // Getter & Setter
     public String getFirstName() {
         return firstName;
     }
-
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
@@ -29,7 +25,6 @@ public class User implements Serializable {
     public String getLastName() {
         return lastName;
     }
-
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
@@ -37,8 +32,42 @@ public class User implements Serializable {
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getSource() {
+        return source;
+    }
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getOffers() {
+        return offers;
+    }
+    public void setOffers(String offers) {
+        this.offers = offers;
+    }
+
+    public String getEmailOffers() {
+        return emailOffers;
+    }
+    public void setEmailOffers(String emailOffers) {
+        this.emailOffers = emailOffers;
+    }
+
+    public String getContactMethod() {
+        return contactMethod;
+    }
+    public void setContactMethod(String contactMethod) {
+        this.contactMethod = contactMethod;
     }
 }
